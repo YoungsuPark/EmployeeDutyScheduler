dd4758dcf0e0580cd516ca9b28ee9ce0f59347ee
YoungsuPark <nice2seeu86@gmail.com>
2016-03-24 11:15:25 +0900

    MailUtil.java 공통유틸 파일 생성

---
+++ DutyScheduleManagement/src/main/java/kr/co/ahope/common/utils/MailUtil.java
@@ -0,0 +1,60 @@
+package kr.co.ahope.common.utils;
+
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import kr.co.ahope.employee.controller.EmplController;
+
+public class MailUtil {
+
+	private static final Logger logger = LoggerFactory.getLogger(EmplController.class);
+	
+	@Autowired
+	private JavaMailSender mailSender;
+
+	private String from ;
+	private String to;
+	private String subject;
+	private String content;
+
+	public void mailSender() {
+
+		try {
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+			messageHelper.setFrom(from);
+			messageHelper.setTo(to);
+			messageHelper.setSubject(subject); 
+			messageHelper.setText(content);
+			
+			mailSender.send(message);
+			
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			logger.debug("email Exception : " + e1);
+		}
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
