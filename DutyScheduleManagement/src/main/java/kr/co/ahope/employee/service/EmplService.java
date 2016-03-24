ee3e68509f2821798156d4e62f75d4916fd7ad6b
YoungsuPark <nice2seeu86@gmail.com>
2016-03-24 11:01:05 +0900

    EmplService 파일 생성

---
+++ DutyScheduleManagement/src/main/java/kr/co/ahope/employee/service/EmplService.java
@@ -0,0 +1,17 @@
+package kr.co.ahope.employee.service;
+
+import java.util.List;
+
+import kr.co.ahope.employee.domain.Employee;
+
+public interface EmplService {
+
+	void addEmpl(Employee empl);
+
+	List<Employee> getEmplList();
+
+	void modifyEmplInfo(Employee empl);
+
+	void deleteEmpl(int emplId);
+
+}
---
+++ DutyScheduleManagement/src/main/java/kr/co/ahope/employee/service/EmplServiceImpl.java
@@ -0,0 +1,37 @@
+package kr.co.ahope.employee.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.ahope.employee.domain.Employee;
+import kr.co.ahope.employee.mapper.EmplMapper;
+
+@Service(value = "emplService")
+public class EmplServiceImpl implements EmplService {
+
+	@Resource(name = "emplMapper")
+	private EmplMapper emplMapper;
+	
+	@Override
+	public List<Employee> getEmplList() {
+		return emplMapper.select();
+	}
+	
+	@Override
+	public void addEmpl(Employee empl) {
+		emplMapper.insert(empl);
+	}
+	
+	@Override
+	public void modifyEmplInfo(Employee empl) {
+		emplMapper.update(empl);
+	}
+	
+	@Override
+	public void deleteEmpl(int emplId) {
+		emplMapper.delete(emplId);
+	}
+}
